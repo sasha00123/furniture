be71a749443da1981592d46f9f28a70662f4d9cb
Alexander Batyrgariev <sasha@bashkort.org>
2020-06-22 23:41:49 +0300

    Added admins

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import send_list
+from main.views import send_list, get_admins
 
 urlpatterns = [
-    path('sendlist', send_list)
+    path('sendlist', send_list),
+    path('admins', get_admins)
 ]
--- main/views.py
+++ main/views.py
@@ -7,3 +7,7 @@ from main.models import TelegramUser
 
 def send_list(request):
     return HttpResponse(json.dumps([{"Telegram ID": user.chat_id} for user in TelegramUser.objects.all()]))
+
+
+def get_admins(request):
+    return HttpResponse(json.dumps([user.chat_id for user in TelegramUser.objects.filter(is_admin=True)]))
